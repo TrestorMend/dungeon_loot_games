3b6d34d35af7487fa8b9e340c89ae1515deea3bf
Marcelo Silva <mdrj10@hotmail.com>
2015-03-30 03:06:59 -0500

    Active Admin installed, and Products and Categories are listed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'products#index'
   
   get '/:id' => 'products#show', as: 'product',  id: /\d+/
