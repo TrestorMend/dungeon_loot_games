3570540b6728c920e24f68fbd1eddbce86e859b7
Marcelo Silva <mdrj10@hotmail.com>
2015-04-22 00:24:36 -0500

    Final commit before RUBOCOP

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,9 +14,22 @@ class CartsController < ApplicationController
     @customer = Client.find(session['client'])
     @order = Order.find(session[:order_id].to_i)
     @order_item = @order.order_items
-    @order.client = @customer
-    @tax = @order.subtotal * (@customer.province.pst) * (@customer.province.gst) * (@customer.province.hst)
+    @order.client = @customer    
+    @tax = @order.subtotal
+    if @customer.province.pst != 0
+      @tax = @tax + (@tax * (@customer.province.pst * 0.01))
+    end
+    if @customer.province.gst != 0
+      @tax = @tax + (@tax * (@customer.province.gst * 0.01))
+    end
+    if @customer.province.hst != 0
+      @tax = @tax + (@tax * (@customer.province.hst * 0.01))
+    end
+    @tax = @tax - @order.subtotal
     @order.tax = @tax
     @order.total = @tax + @order.subtotal
+    @order.save
+    session['client'] = nil
+    session[:order_id] = nil
   end
 end
